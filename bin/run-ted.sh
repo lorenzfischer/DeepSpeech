422e3cc50fcc4ac7f126adcdc9ff0c79e9aebe93
Kelly Davis <kdavis@mozilla.com>
2016-11-21 23:09:16 +0100

    Set train, test, and dev limits to 10k, 100, and 100

--- bin/run-ted.sh
+++ bin/run-ted.sh
@@ -11,11 +11,20 @@ export ds_importer
 ds_batch_size=32
 export ds_batch_size
 
-ds_training_iters=15
+ds_training_iters=300
 export ds_training_iters
 
-ds_validation_step=15
+ds_validation_step=50
 export ds_validation_step
+
+ds_display_step=50
+export ds_display_step
+
+ds_checkpoint_step=50
+export ds_checkpoint_step
+
+ds_use_warpctc=1
+export ds_use_warpctc
 
 if [ ! -f DeepSpeech.ipynb ]; then
     echo "Please make sure you run this from DeepSpeech's top level directory."
