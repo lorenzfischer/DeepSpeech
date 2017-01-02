4699dc9aece4b59e8aea3cc94f06033915b3b0f0
Ubuntu <ubuntu@ip-172-31-47-45.eu-west-1.compute.internal>
2017-01-02 08:50:45 +0000

    experiemnting with settings

--- bin/run-ted-nb.sh
+++ bin/run-ted-nb.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-ds_checkpoint_dir="/mnt/data/ds_checkpoints"
-export ds_checkpoint_dir
+export ds_importer="ted"
+export ds_checkpoint_dir="/mnt/data/ds_checkpoints/$ds_importer"
+export ds_export_dir="/mnt/data/ds_models/$ds_importer"
+export ds_dataset_path="/mnt/data/datasets/$ds_importer"
 
-ds_dataset_path="/mnt/data/datasets/ted"
-export ds_dataset_path
+export ds_limit_train=1
+export ds_limit_dev=1
+export ds_limit_test=1
 
-ds_importer="ted"
-export ds_importer
+export ds_train_batch_size=1 # 16
+export ds_dev_batch_size=1
+export ds_test_batch_size=1
 
-ds_batch_size=32
-export ds_batch_size
+export ds_learning_rate=0.001 # 0.0001
+export ds_validation_step=1 # 20
 
-ds_training_iters=15
-export ds_training_iters
+export ds_epochs=1
+export ds_display_step=1  # 10
+export ds_checkpoint_step=1
 
-ds_validation_step=15
-export ds_validation_step
 
 jupyter-notebook DeepSpeech.ipynb
--- bin/run-ted.sh
+++ bin/run-ted.sh
@@ -3,16 +3,26 @@
 set -xe
 
 export ds_importer="ted"
+export ds_checkpoint_dir="/mnt/data/ds_checkpoints/$ds_importer"
+export ds_export_dir="/mnt/data/ds_models/$ds_importer"
+export ds_dataset_path="/mnt/data/datasets/$ds_importer"
 
-export ds_train_batch_size=16
-export ds_dev_batch_size=8
-export ds_test_batch_size=8
+export ds_restore_checkpoint=1 # 0
 
-export ds_learning_rate=0.0001
-export ds_validation_step=20
+export ds_limit_train=1280
+export ds_limit_dev=1280
+export ds_limit_test=1280
 
-export ds_epochs=150
-export ds_display_step=10
+
+export ds_train_batch_size=20 # 16
+export ds_dev_batch_size=20
+export ds_test_batch_size=20
+
+export ds_learning_rate=0.001 # 0.0001
+export ds_validation_step=1 # 20
+
+export ds_epochs=10
+export ds_display_step=1  # 10
 export ds_checkpoint_step=1
 
 if [ ! -f DeepSpeech.ipynb ]; then
